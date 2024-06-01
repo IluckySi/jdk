e25741e1511ed058f65455e2ff30d4da6f6e9101
ilucky.si <ilucky.si@cloudwise.com>
2024-06-01 16:39:42 +0800

    I_DEBUG

--- src/hotspot/share/runtime/javaCalls.cpp
+++ src/hotspot/share/runtime/javaCalls.cpp
@@ -322,6 +322,7 @@ Handle JavaCalls::construct_new_instance(InstanceKlass* klass, Symbol* construct
 
 
 void JavaCalls::call(JavaValue* result, const methodHandle& method, JavaCallArguments* args, TRAPS) {
+  printf("------------JavaCalls::call---------------------")
   // Check if we need to wrap a potential OS exception handler around thread.
   // This is used for e.g. Win32 structured exception handlers.
   // Need to wrap each and every time, since there might be native code down the
